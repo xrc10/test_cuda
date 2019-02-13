6951e99d567c737e666deb1b87146f801e0ffc54
Ruochen Xu <ruochenx@philly.lti.cs.cmu.edu>
2019-02-13 15:47:56 -0500

    add multiple option

--- test_cuda.py
+++ test_cuda.py
@@ -22,14 +22,22 @@ def get_gpu_memory_map():
     return gpu_memory_map
 
 def main():
+    if len(sys.argv) > 2:
+        n_gpus = int(sys.argv[2])
+    else:
+        n_gpus = 1
+
+    available_gpus = []
     while True:
         interval = 5 # check every 5 seconds
         minimum = int(sys.argv[1])
         mm = get_gpu_memory_map()
         for k,v in sorted(mm.items(), key = lambda x : x[1], reverse = True):
             # print("GPU:{} |||| free: {}".format(k, v))
-            if v > minimum:
-                print(k)
+            if v > minimum and (not k in available_gpus):
+                available_gpus.append(k)
+                if len(available_gpus) >= n_gpus:
+                    print(','.join(available_gpus))
                 sys.exit(0)
         time.sleep(interval)   # Delays for 5 seconds.
 
